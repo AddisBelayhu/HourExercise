21ab95dd178c729edea24a6a0d02879105595d8b
AddisBelayhu <addisgithub@gmail.com>
2023-03-17 14:22:40 +0300

    houre watch

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-js
+let hrs = document.querySelector("#h");
+let min = document.querySelector("#m");
+let scs = document.querySelector("#s");
+
+setInterval(()=>{
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * 6;
+    let ss = day.getSeconds() * 6;
+
+    hrs.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+    mns.style.transform = `rotateZ(${mm}deg)`;
+    scs.style.transform = `rotateZ(${ss}deg)`;
+})
